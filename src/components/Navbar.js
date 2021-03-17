1ac4193a097d08f41796cc7fb39be7568a3155eb
Jake Jenkins <jake@speedyweb.dev>
2021-03-17 14:52:49 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
 
     return (
         <nav>
-        <h1><img src="jnx-logo.png" alt="JNX logo"></img></h1>
+        <h1><img src="/jnx-logo.png" alt="JNX logo"></img></h1>
         <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
