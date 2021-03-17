d5e70ac460ff2690a3d1fd5d8f1d0682fe9e147a
Jake Jenkins <jake@speedyweb.dev>
2021-03-17 02:05:37 +0000

    create

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../components/Layout"
+
+export default function NotFound() {
+    return (
+        <Layout>
+        <div>
+            <h2>Not Found</h2>
+            <p>Sorry the file was not found.</p>
+        </div>
+        </Layout>
+    )
+}
