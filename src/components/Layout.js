97a76a61d5a25b52d327f326272c3799f088d962
Jake Jenkins <hello@walesdev.com>
2021-06-22 21:23:20 +0100

    Update Layout.js
    Add analytics

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,13 +5,14 @@ import '../styles/global.css'
 export default function Layout({ children }) {
     return (
         <div>
-           <Navbar />
-        <main>
-            {children}
-        </main>
-        <footer>
-        WalesDev.com Web Developer in Wales © Jake Jenkins 2003 - 2021.
-        </footer>
+            <Navbar />
+            <main>
+                {children}
+            </main>
+            <footer>
+                WalesDev.com Web Developer in Wales © Jake Jenkins 2003 - 2021.
+            </footer>
+            <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "0540619f98104afab3f3eac71fe36435"}'></script>
         </div>
     )
 }
